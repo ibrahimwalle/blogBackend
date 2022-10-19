d3de5bc2554e6f00194ef97395f7ce61000f6f26
Ibrahim Wali <dinowali9@gmail.com>
2022-10-19 22:33:30 +0100

    Dockerfiles

--- src/config/mongo.config.js
+++ src/config/mongo.config.js
@@ -1,6 +1,6 @@
 //Mongo DB configuration variables
 export const mongoConfig = {
-    uri: 'mongodb://localhost:27017',
+    uri: 'mongodb://mongodb:27017',
     user: 'admin',
     pass: 'password',
     dbName: 'blog-app'
